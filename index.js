aa73589a01a156f814919cad20513117e886d2d6
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-03 21:23:15 -0300

    added function to pull answers using require and fs to write data. Added list of questions for the generator

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 // Packages needed for this application - inquirer and fs
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./lib/generateSvg');
 
-
-// An array of questions to prompt for user input
-
-//GIVEN a command-line application that accepts user input
+inquirer
+  .prompt([
 //WHEN I am prompted for text
-//THEN I can enter up to three characters
+//THEN I can enter up to three characters <--IMPORTANT (need then statements to handle input)
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Please enter three letters',
+    },
 //WHEN I am prompted for the text color
-//THEN I can enter a color keyword (OR a hexadecimal number)
+//THEN I can enter a color keyword (OR a hexadecimal number) <--IMPORTANT
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Pick a color for your Text!',
+    },
 //WHEN I am prompted for a shape
 //THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Pick a shape',
+      choices: ['Circle', 'Triangle', 'Square']
+    },
 //WHEN I am prompted for the shape's color
-//THEN I can enter a color keyword (OR a hexadecimal number)
+//THEN I can enter a color keyword (OR a hexadecimal number) <--IMPORTANT
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Pick a color for your Shape!',
+    },
+  ])
+
 //WHEN I have entered input for all the prompts
 //THEN an SVG file is created named `logo.svg`
 //AND the output text "Generated logo.svg" is printed in the command line
-//WHEN I open the `logo.svg` file in a browser
-//THEN I am shown a 300x200 pixel image that matches the criteria I entered
+  .then((answers) => {
+    const svgContent = generateSvg(answers);
 
-
-
-// write a function to initialize app and write file
+    fs.writeFile('logo.svg', svgContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created logo.svg!')
+    );
+  });
