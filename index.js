40710bebc74e80d56a370dad8e1e9b46f52210d6
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-10 18:52:20 -0300

    added comments, tried another way to write new files but it didnt work

--- index.js
+++ index.js
@@ -6,30 +6,22 @@ const {Shape, Circle, Triangle, Square} = require('./lib/shapes.js');
 
 inquirer
   .prompt([
-//WHEN I am prompted for text
-//THEN I can enter up to three characters <--IMPORTANT (need then statements to handle input)
     {
       type: 'input',
       name: 'text',
       message: 'Please enter three letters',
     },
-//WHEN I am prompted for the text color
-//THEN I can enter a color keyword (OR a hexadecimal number) <--IMPORTANT
     {
       type: 'input',
       name: 'textColor',
       message: 'Pick a color for your Text!',
     },
-//WHEN I am prompted for a shape
-//THEN I am presented with a list of shapes to choose from: circle, triangle, and square
     {
       type: 'list',
       name: 'shape',
       message: 'Pick a shape',
       choices: ['Circle', 'Triangle', 'Square']
     },
-//WHEN I am prompted for the shape's color
-//THEN I can enter a color keyword (OR a hexadecimal number) <--IMPORTANT
     {
       type: 'input',
       name: 'shapeColor',
@@ -37,9 +29,7 @@ inquirer
     },
   ])
 
-//WHEN I have entered input for all the prompts
-//THEN an SVG file is created named `logo.svg`
-//AND the output text "Generated logo.svg" is printed in the command line
+//Function to render shapes
   .then((answers) => {
     let shape;
     switch (answers.shape) {
@@ -56,6 +46,7 @@ inquirer
             break
     }
 
+//Function to write file
     const svgContent = shape.renderShape()
     fs.writeFile(`examples/logo.svg`, svgContent, err => {
         err ? console.error(err) : console.info('Successfully created logo.svg!!!')
@@ -64,5 +55,3 @@ inquirer
 .catch(error => {
     console.error(error)
 })
-
-
